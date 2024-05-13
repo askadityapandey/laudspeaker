d01f1273e367fbfa0b66d1d233bccdd46f775f5d
Mahamad Charawi <mahamad@laudspeaker.com>
2024-05-13 16:33:31 -0700

    Fixing segment tests.

--- packages/tests/cypress/test-helpers/createNewDynamicSegment.ts
+++ packages/tests/cypress/test-helpers/createNewDynamicSegment.ts
@@ -352,6 +352,10 @@ export const createNewDynamicSegment = ({
   cy.contains("users estimated reached ≈ 1").should("be.visible");
 
   cy.get("#saveSegmentButton").click();
+  cy.wait(2000);
+  cy.visit("/segment");
+  cy.wait(2000);
+  cy.contains(name).click();
   cy.contains("Eligible users: 1 Users").should("be.visible");
   cy.wait(2000);
 };
