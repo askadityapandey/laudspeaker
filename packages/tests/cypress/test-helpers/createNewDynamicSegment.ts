e8b6099e7d6bfa3353b080bb8b69152767e3ab34
Mahamad Charawi <mahamad@laudspeaker.com>
2024-05-15 12:48:21 -0700

    Increasing timeout for segment saving.

--- packages/tests/cypress/test-helpers/createNewDynamicSegment.ts
+++ packages/tests/cypress/test-helpers/createNewDynamicSegment.ts
@@ -352,9 +352,8 @@ export const createNewDynamicSegment = ({
   cy.contains("users estimated reached ≈ 1").should("be.visible");
 
   cy.get("#saveSegmentButton").click();
-  cy.wait(2000);
+  cy.wait(10000);
   cy.visit("/segment");
-  cy.wait(2000);
   cy.contains(name).click();
   cy.contains("Eligible users: 1 Users").should("be.visible");
   cy.wait(2000);
