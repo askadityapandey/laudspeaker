c66ae286c292df336979da91667017c972c546e8
Moustafa Eid <eidmoustafa@gmail.com>
2024-05-23 19:42:33 -0700

    reset tests

--- packages/tests/cypress/e2e/020-audience-segments-create-segment-dynamic-rules.spec.cy.ts
+++ packages/tests/cypress/e2e/020-audience-segments-create-segment-dynamic-rules.spec.cy.ts
@@ -1,58 +1,58 @@
-// import credentials from "../fixtures/credentials";
-// import signup from "../test-helpers/signup";
-// import { setupOrganization } from "../test-helpers/setupOrganization";
-// import { uploadCSV } from "../test-helpers/uploadCSV";
-// import { mapAttributesToNewFields } from "../test-helpers/mapAttributesToNewFields";
-// import {
-//   booleanSegments,
-//   createNewDynamicSegment,
-//   emailSegments,
-//   numberSegments,
-//   stringSegments,
-// } from "../test-helpers/createNewDynamicSegment";
+import credentials from "../fixtures/credentials";
+import signup from "../test-helpers/signup";
+import { setupOrganization } from "../test-helpers/setupOrganization";
+import { uploadCSV } from "../test-helpers/uploadCSV";
+import { mapAttributesToNewFields } from "../test-helpers/mapAttributesToNewFields";
+import {
+  booleanSegments,
+  createNewDynamicSegment,
+  emailSegments,
+  numberSegments,
+  stringSegments,
+} from "../test-helpers/createNewDynamicSegment";
 
-// const { email, password, firstName, lastName, organizationName, timeZone } =
-//   credentials;
+const { email, password, firstName, lastName, organizationName, timeZone } =
+  credentials;
 
-// describe("Segment Correctness", { retries: 2 }, () => {
-//   beforeEach(() => {
-//     cy.request(`${Cypress.env("TESTS_API_BASE_URL")}/tests/reset-tests`);
-//     cy.wait(1000);
-//     cy.clearAllCookies();
-//     cy.clearAllLocalStorage();
-//     cy.clearAllSessionStorage();
-//     signup(email, password, firstName, lastName);
-//     cy.wait(1000);
-//   });
+describe("Segment Correctness", { retries: 2 }, () => {
+  beforeEach(() => {
+    cy.request(`${Cypress.env("TESTS_API_BASE_URL")}/tests/reset-tests`);
+    cy.wait(1000);
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+    cy.clearAllSessionStorage();
+    signup(email, password, firstName, lastName);
+    cy.wait(1000);
+  });
 
-//   it("works as expected", () => {
-//     setupOrganization(organizationName, timeZone);
+  it("works as expected", () => {
+    setupOrganization(organizationName, timeZone);
 
-//     cy.wait(10000);
-//     cy.visit("/home");
-//     cy.url().should("include", "/home");
+    cy.wait(10000);
+    cy.visit("/home");
+    cy.url().should("include", "/home");
 
-//     uploadCSV("correctness_testing.csv", mapAttributesToNewFields, 0);
+    uploadCSV("correctness_testing.csv", mapAttributesToNewFields, 0);
 
-//     // Create string segments
-//     stringSegments.forEach(createNewDynamicSegment);
-//     cy.wait(5000);
+    // Create string segments
+    stringSegments.forEach(createNewDynamicSegment);
+    cy.wait(5000);
 
-//     // Create number segments
-//     numberSegments.forEach(createNewDynamicSegment);
-//     cy.wait(5000);
+    // Create number segments
+    numberSegments.forEach(createNewDynamicSegment);
+    cy.wait(5000);
 
-//     // Create boolean segments
-//     booleanSegments.forEach(createNewDynamicSegment);
-//     cy.wait(5000);
+    // Create boolean segments
+    booleanSegments.forEach(createNewDynamicSegment);
+    cy.wait(5000);
 
-//     // create email segments
-//     emailSegments.forEach(createNewDynamicSegment);
-//     cy.wait(5000);
+    // create email segments
+    emailSegments.forEach(createNewDynamicSegment);
+    cy.wait(5000);
 
-//     // create date segments
-//     // FIXME: date comparisons are failing despite having users
-//     // matching the conditions
-//     // dateSegments.forEach(createNewDynamicSegment);
-//   });
-// });
+    // create date segments
+    // FIXME: date comparisons are failing despite having users
+    // matching the conditions
+    // dateSegments.forEach(createNewDynamicSegment);
+  });
+});
---
+++ packages/tests/cypress/e2e/032-customer-upsert-tests.cy.ts
@@ -0,0 +1,162 @@
+// cypress tests fail after 30 mins in pipeline. enable this after we investigate
+// import credentials from "../fixtures/credentials";
+// import createTestCustomer from "../test-helpers/createTestCustomer";
+// import drag from "../test-helpers/drag";
+// import { loginFunc } from "../test-helpers/loginFunc";
+// import setMailgun from "../test-helpers/setMailgun";
+// import { setupOrganization } from "../test-helpers/setupOrganization";
+// import signup from "../test-helpers/signup";
+// import uuid from "../test-helpers/uuid";
+// import { uuid } from 'uuidv4';
+// import { createPrimaryKey } from "../test-helpers/createPrimaryKey";
+
+// const { email, password, firstName, lastName, organizationName, timeZone } =
+//   credentials;
+
+// describe("Customer upsert 3", { retries: 2 }, () => {
+//   beforeEach(() => {
+//     cy.request(`${Cypress.env("TESTS_API_BASE_URL")}/tests/reset-tests`);
+//     cy.wait(1000);
+//     cy.clearAllCookies();
+//     cy.clearAllLocalStorage();
+//     cy.clearAllSessionStorage();
+//     signup(email, password, firstName, lastName);
+//     cy.wait(1000);
+//   });
+
+//   it("doesn't create duplicates using phone_number", () => {
+//     cy.viewport(1920, 1080);
+//     setupOrganization(organizationName, timeZone);
+//     cy.get("#settings").click();
+//     cy.contains("API").click();
+//     cy.wait(1000);
+
+//     const uuid1 = uuid();
+
+//     cy.get("#privateAPIKey")
+//       .invoke("val")
+//       .then((val) => {
+//         const apikey = val?.toString() || "no";
+//         createTestCustomer(email, ["phone_number"]);
+//         cy.wait(1000);
+
+//         cy.request({
+//           method: "POST",
+//           url: `${Cypress.env("TESTS_API_BASE_URL")}/events/batch`,
+//           headers: { Authorization: `Api-Key ${apikey}` },
+//           body: {
+//             batch: [
+//               {
+//                 timestamp: "2024-03-15T02:31:05.295Z",
+//                 uuid: "F451DF0A-D713-4076-AE20-41AB1641BC98",
+//                 event: "$identify",
+//                 source: "mobile",
+//                 correlationKey: "_id",
+//                 payload: {
+//                   $anon_distinct_id: uuid1,
+//                   distinct_id: email,
+//                   phone_number: "123-456-7890",
+//                 },
+//                 correlationValue: uuid1,
+//               }
+//             ],
+//           },
+//         }).then(() => {
+//           cy.wait(1000);
+
+//           cy.request({
+//             method: "POST",
+//             url: `${Cypress.env("TESTS_API_BASE_URL")}/customers/upsert`,
+//             headers: { Authorization: `Api-Key ${apikey}` },
+//             body: {
+//               primary_key: "john@smith.com",
+//               properties: {
+//                 phone_number: "123-456-7890",
+//               }
+//             },
+//           }).then(() => {
+//             cy.wait(1000);
+
+//             cy.visit("/people");
+
+//             cy.get('.id-cell').should('have.length', 1);
+
+//             cy.get(".id-cell:first").click();
+//             cy.contains(uuid1).should("exist");
+//             cy.contains("phone_number").should("exist");
+//             cy.contains("123-456-7890").should("exist");
+//           });
+//         });
+//       });
+//   });
+
+//   it("doesn't create duplicates using phone_number_with_ext", () => {
+//     cy.viewport(1920, 1080);
+//     setupOrganization(organizationName, timeZone);
+//     cy.get("#settings").click();
+//     cy.contains("API").click();
+//     cy.wait(1000);
+
+//     const uuid1 = uuid();
+
+//     cy.get("#privateAPIKey")
+//       .invoke("val")
+//       .then((val) => {
+//         const apikey = val?.toString() || "no";
+//         createTestCustomer(email, ["phone_number_with_ext"]);
+//         cy.wait(1000);
+
+//         cy.request({
+//           method: "POST",
+//           url: `${Cypress.env("TESTS_API_BASE_URL")}/events/batch`,
+//           headers: { Authorization: `Api-Key ${apikey}` },
+//           body: {
+//             batch: [
+//               {
+//                 timestamp: "2024-03-15T02:31:05.295Z",
+//                 uuid: "F451DF0A-D713-4076-AE20-41AB1641BC98",
+//                 event: "$identify",
+//                 source: "mobile",
+//                 correlationKey: "_id",
+//                 payload: {
+//                   $anon_distinct_id: uuid1,
+//                   distinct_id: email,
+//                   phone_number_with_ext: "+1 000-111-2222",
+//                 },
+//                 correlationValue: uuid1,
+//               }
+//             ],
+//           },
+//         }).then(() => {
+//           cy.wait(1000);
+
+//           cy.request({
+//             method: "POST",
+//             url: `${Cypress.env("TESTS_API_BASE_URL")}/customers/upsert`,
+//             headers: { Authorization: `Api-Key ${apikey}` },
+//             body: {
+//               primary_key: "john@smith.com",
+//               properties: {
+//                 phone_number_with_ext: "+1 000-111-2222",
+//               }
+//             },
+//           }).then(() => {
+//             cy.wait(1000);
+
+//             cy.visit("/people");
+
+//             cy.get('.id-cell').should('have.length', 1);
+
+//             cy.get(".id-cell:first").click();
+//             cy.contains(uuid1).should("exist");
+//             cy.contains("phone_number_with_ext").should("exist");
+//             cy.contains("+1 000-111-2222").should("exist");
+//           });
+//         });
+//       });
+//   });
+
+// });
+
+
+
