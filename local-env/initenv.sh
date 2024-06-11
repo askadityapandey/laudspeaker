02e7656ed5750adf5ada858613f8736b46083482
Abheek Basu <abheek94@gmail.com>
2024-06-10 21:16:23 -0400

    set up

--- local-env/initenv.sh
+++ local-env/initenv.sh
@@ -6,21 +6,21 @@ arguments=("$@") # options include [environment, connectors, guis]
 if [[ ${arguments[@]} =~ "environment" ]]; then
   echo "Setting up 'environment' resources."
   # Initialize mongo replicaset
-  mongosh "mongodb://mongo:27017/nest?directConnection=true" --eval "rs.initiate()"
+  # mongosh "mongodb://mongo:27017/nest?directConnection=true" --eval "rs.initiate()"
 fi
 
 if [[ ${arguments[@]} =~ "connectors" ]]; then
   echo "Setting up 'connectors' resources."
   # Setup kafka connector
-  curl kafka-connect1:8083/connectors -X POST -H "Content-Type: application/json" --data '{
-   "name": "mongo-simple-source",
-   "config": {
-     "connector.class": "com.mongodb.kafka.connect.MongoSourceConnector",
-     "connection.uri": "mongodb://mongo:27017/nest?directConnection=true",
-     "database": "nest",
-     "collection": "customers"
-   }
-  }'
+  # curl kafka-connect1:8083/connectors -X POST -H "Content-Type: application/json" --data '{
+  #  "name": "mongo-simple-source",
+  #  "config": {
+  #    "connector.class": "com.mongodb.kafka.connect.MongoSourceConnector",
+  #    "connection.uri": "mongodb://mongo:27017/nest?directConnection=true",
+  #    "database": "nest",
+  #    "collection": "customers"
+  #  }
+  # }'
 
   curl kafka-connect1:8083/connectors -X POST -H "Content-Type: application/json" --data '{
     "name": "clickhouse-sink",
