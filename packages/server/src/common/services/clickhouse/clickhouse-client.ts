96a9e985cc1f37a4a71b008433b0fb12830ed258
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2024-07-24 19:00:31 -0700

    init

--- packages/server/src/common/services/clickhouse/clickhouse-client.ts
+++ packages/server/src/common/services/clickhouse/clickhouse-client.ts
@@ -1,3 +1,32 @@
+import { createClient } from '@clickhouse/client';
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
 export class ClickhouseClient {
-	
+	private client = createClient({
+	  host: process.env.CLICKHOUSE_HOST
+	    ? process.env.CLICKHOUSE_HOST.includes('http')
+	      ? process.env.CLICKHOUSE_HOST
+	      : `http://${process.env.CLICKHOUSE_HOST}`
+	    : 'http://localhost:8123',
+	  username: process.env.CLICKHOUSE_USER ?? 'default',
+	  password: process.env.CLICKHOUSE_PASSWORD ?? '',
+	  database: process.env.CLICKHOUSE_DB ?? 'default',
+	});
+
+	constructor() {
+
+	}
+
+	async init() {
+
+	}
+
+	async query(queryDetails) {
+		return this.client.query(queryDetails);
+	}
+
+	async insert(insertDetails) {
+		return this.client.insert(insertDetails);
+	}
 }
